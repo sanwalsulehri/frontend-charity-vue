dce812842bc64c29ff4d2b9a0e38e6d2e2d487da
azharmushtaq0099 <azharmushtaq344@gmail.com>
2024-12-23 19:02:51 +0500

    error resolve

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,5 @@
-/* eslint-disable no-undef */
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath: '/'
 });
-/* eslint-enable no-undef */
