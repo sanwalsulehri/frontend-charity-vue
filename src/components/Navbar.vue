febbd6d92d0df64e97f064dab5c5223cfaf41b4a
azharmushtaq0099 <azharmushtaq344@gmail.com>
2024-12-24 16:01:07 +0500

    done About fixes

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,23 +6,7 @@
   >
     <div class="mx-auto max-w-7xl w-full px-2 sm:px-6 lg:px-8">
       <div class="relative w-full flex h-16 items-center justify-between">
-        <div class="absolute sm:w-0 inset-y-0 left-0 flex items-center sm:hidden">
-          <!-- Mobile menu button-->
-          <DisclosureButton class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-            <span class="absolute -inset-0.5" />
-            <span class="sr-only">Open main menu</span>
-            <Bars3Icon
-              v-if="!open"
-              class="block size-6"
-              aria-hidden="true"
-            />
-            <XMarkIcon
-              v-else
-              class="block size-6"
-              aria-hidden="true"
-            />
-          </DisclosureButton>
-        </div>
+      
         <div class="flex flex-1 items-center sm:items-stretch sm:justify-start">
           <div class="flex shrink-0 items-center">
             <a href="/">
@@ -51,12 +35,12 @@
         <div class="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
           <button
             type="button"
-            class="relative rounded-full mr-3 bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+            class="relative rounded-full sm:mr-3 mr-1 bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
           >
             <span class="absolute -inset-1.5" />
             <span class="sr-only">View notifications</span>
             <BellIcon
-              class="size-6"
+              class="sm:size-6 size-4"
               aria-hidden="true"
             />
           </button>
@@ -64,12 +48,12 @@
 
           <button
             type="button"
-            class="relative rounded-full mr-3 bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+            class="relative rounded-full sm:mr-3 mr-1 bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
           >
             <span class="absolute -inset-1.5" />
             <span class="sr-only">View notifications</span>
             <ShoppingBagIcon
-              class="size-6"
+              class="size-4 sm:size-6"
               aria-hidden="true"
             />
             <div class="absolute top-0 text-xs bg-gray-700 right-0 size-4 rounded-full">
@@ -90,7 +74,7 @@
                 <span class="absolute -inset-1.5" />
                 <span class="sr-only">Open user menu</span>
                 <img
-                  class="size-8 rounded-full"
+                  class="size-6 sm:size-8 rounded-full"
                   src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
                   alt=""
                 >
@@ -126,7 +110,23 @@
               </MenuItems>
             </transition>
           </Menu>
-          
+          <div class=" sm:w-0 inset-y-0 left-0 flex items-center sm:hidden">
+          <!-- Mobile menu button-->
+          <DisclosureButton class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+            <span class="absolute -inset-0.5" />
+            <span class="sr-only">Open main menu</span>
+            <Bars3Icon
+              v-if="!open"
+              class="block size-6"
+              aria-hidden="true"
+            />
+            <XMarkIcon
+              v-else
+              class="block size-6"
+              aria-hidden="true"
+            />
+          </DisclosureButton>
+        </div>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,20 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        leScaleXIn: {
+          from: {
+            transform: 'perspective(400px) rotateX(90deg)',
+            opacity: '0',
+          },
+          '50%': { opacity: '1' },
+        },
+      },
+      animation: {
+        leScaleXIn: 'leScaleXIn 1s ease-in-out', // Customize the duration and timing
+      },
+    },
   },
   plugins: [],
-}
+};
