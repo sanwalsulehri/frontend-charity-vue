4c17bc8dd77cb6cf350ce7928ec0f84e11038a13
sanwalsulehri <sanwalsulehri077@gmail.com>
2024-12-24 15:19:48 +0500

    font

--- src/main.js
+++ src/main.js
@@ -8,6 +8,7 @@ import * as components from 'vuetify/components';
 import * as directives from 'vuetify/directives';
 import axios from 'axios';
 import './assets/index.css'
+import './style.css'
 
 const vuetify = createVuetify({
   components,
