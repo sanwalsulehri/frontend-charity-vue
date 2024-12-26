3a3d4b091318e08714e5c4bd74f12eedf2272edc
kashifal <captainkashi4@gmail.com>
2024-12-26 22:42:52 +0500

    amazing

--- src/views/CompetitionDetailPage.vue
+++ src/views/CompetitionDetailPage.vue
@@ -103,7 +103,7 @@ import axios from "axios";
 import Navbar from "../components/Navbar.vue";
 import { useRoute } from "vue-router";
 import { useStore } from "vuex";
-import { RadioGroup, RadioGroupOption } from "@headlessui/vue";
+ 
 import Toastify from 'toastify-js';
 import 'toastify-js/src/toastify.css';
 
@@ -117,47 +117,9 @@ import 'toastify-js/src/toastify.css';
 const competition = ref({});
 const quantity = ref(1);
 const competitionImage = ref("");
-const product = {
-  name: "Basic Tee",
-  price: "$35",
-  rating: 3.9,
-  reviewCount: 512,
-  href: "#",
-  breadcrumbs: [
-    { id: 1, name: "Women", href: "#" },
-    { id: 2, name: "Clothing", href: "#" },
-  ],
-  images: [
-    {
-      id: 1,
-      imageSrc:
-        "https://tailwindui.com/plus/img/ecommerce-images/product-page-01-featured-product-shot.jpg",
-      imageAlt: "Back of women's Basic Tee in black.",
-      primary: true,
-    },
-  ],
+ 
 
-  sizes: [
-    { name: "XXS", inStock: true },
-    { name: "XS", inStock: true },
-    { name: "S", inStock: true },
-    { name: "M", inStock: true },
-    { name: "L", inStock: true },
-    { name: "XL", inStock: false },
-  ],
-  description: `
-    <p>The Basic tee is an honest new take on a classic. The tee uses super soft, pre-shrunk cotton for true comfort and a dependable fit. They are hand cut and sewn locally, with a special dye technique that gives each tee it's own look.</p>
-    <p>Looking to stock your closet? The Basic tee also comes in a 3-pack or 5-pack at a bundle discount.</p>
-  `,
-  details: [
-    "Only the best materials",
-    "Ethically and locally made",
-    "Pre-washed and pre-shrunk",
-    "Machine wash cold with similar colors",
-  ],
-};
-
-const selectedSize = ref(product.sizes[2]);
+// const selectedSize = ref(product.sizes[2]);
 
 // Vuex and Vue Router
 const store = useStore();
